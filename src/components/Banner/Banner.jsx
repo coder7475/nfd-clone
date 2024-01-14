af954dd1cc4879bebac279b3259afe44d9369f7e
coder7475 <robiulhossain7475@gmail.com>
2024-01-14 10:43:53 +0600

    feat: add banner image

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,12 @@
+
+const Banner = () => {
+    return (
+        <div className="bg-white max-w-[95%] mx-auto rounded-[30px] mt-3 py-5 flex justify-center items-center">
+            <figure>
+                <img src="/rabbit.svg" alt="nfd rabbit" />
+            </figure>     
+        </div>
+    );
+};
+
+export default Banner;
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Navbar from "../shared/Navbar";
+import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
         <>
             <Navbar/>
+            <Banner/>
         </>
     );
 };
