9e414c2e55cc988b2765584aaccbf0fabba77502
coder7475 <robiulhossain7475@gmail.com>
2024-01-14 20:29:12 +0600

    feat: create banner buttons

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,18 +1,31 @@
+import HowItWorks from "../Buttons/HowItWorks";
+import OnlyPCVersion from "../Buttons/OnlyPCVersion";
+
+function BannerHeader() {
+  return (
+    <div className="syne text-center text-[36px] font-bold">
+      <h1 className="">NFD platform</h1>
+      <h2 className="text-[#848AFF]">“NFD.gg”</h2>
+    </div>
+  );
+}
 
 const Banner = () => {
   return (
-    <section >
-      <div className="bg-white max-w-[95%] mx-auto rounded-[30px] mt-3 py-5 flex justify-center items-center flex-col gap-6">
+    <section>
+      <div className="bg-white max-w-[616px] mx-auto rounded-[30px] mt-3 py-5 flex justify-center items-center flex-col gap-6">
         <figure>
           <img src="/rabbit.svg" alt="nfd rabbit" />
         </figure>
-        <div className="syne text-center text-[36px] font-bold">
-            <h1 className="">NFD platform</h1>
-            <h2 className="text-[#848AFF]">“NFD.gg”</h2>
-        </div>
+        <BannerHeader></BannerHeader>
         <p className="text-center w-[568px] text-md text-[#4F5662]">
-        An Alpha community 2.0 with its own platform for participating, searching, and tracking any crypto related information.
+          An Alpha community 2.0 with its own platform for participating,
+          searching, and tracking any crypto related information.
         </p>
+        <div>
+          <OnlyPCVersion/>
+          <HowItWorks />
+        </div>
       </div>
     </section>
   );
---
+++ src/components/Buttons/HowItWorks.jsx
@@ -0,0 +1,11 @@
+const HowItWorks = () => {
+  return (
+    <>
+      <button className="py-2.5 px-5 me-2 mb-2 text-[17px] font-medium text-[#6C52EE] bg-white rounded-lg focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+        How it works?
+      </button>
+    </>
+  );
+};
+
+export default HowItWorks;
