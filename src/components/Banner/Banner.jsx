9ef0dac79a3b82e7085ace29598d164949bf7c9f
coder7475 <robiulhossain7475@gmail.com>
2024-01-14 11:18:02 +0600

    feat: set banner font family to syne

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,12 +1,18 @@
 
 const Banner = () => {
-    return (
-        <div className="bg-white max-w-[95%] mx-auto rounded-[30px] mt-3 py-5 flex justify-center items-center">
-            <figure>
-                <img src="/rabbit.svg" alt="nfd rabbit" />
-            </figure>     
+  return (
+    <section className="syne">
+      <div className="bg-white max-w-[95%] mx-auto rounded-[30px] mt-3 py-5 flex justify-center items-center flex-col gap-2">
+        <figure>
+          <img src="/rabbit.svg" alt="nfd rabbit" />
+        </figure>
+        <div>
+            <h1 className="text-[36px] font-medium">NFD platform</h1>
+            <blockquote className="text-xl">“NFD.gg”</blockquote>
         </div>
-    );
+      </div>
+    </section>
+  );
 };
 
 export default Banner;
