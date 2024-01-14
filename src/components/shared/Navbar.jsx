5452cfb22f37b2d35e1ab314bc83c74642270938
coder7475 <robiulhossain7475@gmail.com>
2024-01-14 06:57:12 +0600

    feat: update favicon of page

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,21 +1,23 @@
+function NavLogo() {
+  return (
+    <a
+      href="https://flowbite.com/"
+      className="flex items-center space-x-3 rtl:space-x-reverse"
+    >
+      <svg width="56" height="43" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 21.061h11.205v10.97H0V21.06Z" fill="#D6B0FF"></path><path d="m17.928 25.449-6.723 6.582L22.41 42.78 34.51 30.934l-1.793-17.551h-14.79v12.066Z" fill="url(#Logo_svg__a)"></path><path d="M11.205 32.03H22.41V43H11.205V32.03Z" fill="#D6B0FF"></path><path d="M48.63 17.332v9.653L32.717 11.408 43.923 0l10.981 10.97-6.275 6.362Z" fill="url(#Logo_svg__b)"></path><path d="M43.923 0h11.205v10.97H43.923V0ZM11.653 10.828 22.428.279l32.644 31.957-10.776 10.549-32.643-31.957Z" fill="#D6B0FF"></path><defs><linearGradient id="Logo_svg__a" x1="29.059" y1="52.563" x2="13.838" y2="-7.019" gradientUnits="userSpaceOnUse"><stop offset="0.031" stopColor="#848AFF"></stop><stop offset="1" stopColor="#EA8CFF"></stop></linearGradient><linearGradient id="Logo_svg__b" x1="29.059" y1="52.563" x2="13.838" y2="-7.019" gradientUnits="userSpaceOnUse"><stop offset="0.031" stopColor="#848AFF"></stop><stop offset="1" stopColor="#EA8CFF"></stop></linearGradient></defs></svg>
+      <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+        Flowbite
+      </span>
+    </a>
+  );
+}
+
 const Navbar = () => {
   return (
-    <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+    <nav className="xl:bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a
-          href="https://flowbite.com/"
-          className="flex items-center space-x-3 rtl:space-x-reverse"
-        >
-          <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            className="h-8"
-            alt="Flowbite Logo"
-          />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Flowbite
-          </span>
-        </a>
-        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">          
+        <NavLogo></NavLogo>
+        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
           <button
             data-collapse-toggle="navbar-sticky"
             type="button"
