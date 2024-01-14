e119275a34712ba3e0a6a6fdfae834517235d1b4
coder7475 <robiulhossain7475@gmail.com>
2024-01-14 11:30:14 +0600

    feat: add paragraph text with proper typography

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,15 +1,18 @@
 
 const Banner = () => {
   return (
-    <section className="syne">
+    <section >
       <div className="bg-white max-w-[95%] mx-auto rounded-[30px] mt-3 py-5 flex justify-center items-center flex-col gap-6">
         <figure>
           <img src="/rabbit.svg" alt="nfd rabbit" />
         </figure>
-        <div className="text-center text-[36px] font-bold">
+        <div className="syne text-center text-[36px] font-bold">
             <h1 className="">NFD platform</h1>
-            <blockquote className="text-[#848AFF]">“NFD.gg”</blockquote>
+            <h2 className="text-[#848AFF]">“NFD.gg”</h2>
         </div>
+        <p className="text-center w-[568px] text-md text-[#4F5662]">
+        An Alpha community 2.0 with its own platform for participating, searching, and tracking any crypto related information.
+        </p>
       </div>
     </section>
   );
