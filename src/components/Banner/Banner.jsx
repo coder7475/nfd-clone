ca052d2e70763500aec65be81889e148524530c4
coder7475 <robiulhossain7475@gmail.com>
2024-01-14 11:24:14 +0600

    feat: add main heading and sub headings

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,13 +2,13 @@
 const Banner = () => {
   return (
     <section className="syne">
-      <div className="bg-white max-w-[95%] mx-auto rounded-[30px] mt-3 py-5 flex justify-center items-center flex-col gap-2">
+      <div className="bg-white max-w-[95%] mx-auto rounded-[30px] mt-3 py-5 flex justify-center items-center flex-col gap-6">
         <figure>
           <img src="/rabbit.svg" alt="nfd rabbit" />
         </figure>
-        <div>
-            <h1 className="text-[36px] font-medium">NFD platform</h1>
-            <blockquote className="text-xl">“NFD.gg”</blockquote>
+        <div className="text-center text-[36px] font-bold">
+            <h1 className="">NFD platform</h1>
+            <blockquote className="text-[#848AFF]">“NFD.gg”</blockquote>
         </div>
       </div>
     </section>
